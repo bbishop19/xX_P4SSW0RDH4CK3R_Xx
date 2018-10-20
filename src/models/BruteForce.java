7e11c452d7a970aa6699137536669dec0e9500b3
bbishop19 <bbishop19@windwardschool.org>
2018-10-19 20:38:31 -0700

    finish WindwardHacker and add interface for guessers + additional tidying

--- src/models/BruteForce.java
+++ src/models/BruteForce.java
@@ -3,12 +3,13 @@ package models;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class BruteForce {
-	ArrayList<Integer> charInts = new ArrayList<Integer>(Arrays.asList(17, 19, 20, 21, 22, 24, 25, 26, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 64, 94));
-	char[] charset;
-	int currentLength = 1;
-	int[] currentGuesses = {0};
-	String guess = "";
+public class BruteForce implements PasswordGuesser{
+	private ArrayList<Integer> charInts = new ArrayList<Integer>(Arrays.asList(17, 19, 20, 21, 22, 24, 25, 26, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 64, 94));
+	private char[] charset;
+	private int currentLength = 1;
+	private int[] currentGuesses = {0};
+	private String guess = "";
+
 	public BruteForce(){
 		for(int i = 65; i < 91; i++){
 			charInts.add(i);
@@ -22,7 +23,7 @@ public class BruteForce {
 		}
 	}
 	
-	public String getNextAttempt(){
+	public String getNext(){
 		guess = "";
 		for(Integer i : currentGuesses){
 			guess += charset[i];
--- src/models/WindwardHacker.java
+++ src/models/WindwardHacker.java
@@ -1,9 +1,48 @@
 package models;
 
-import java.io.File;
+import java.time.Month;
 
-public class WindwardHacker {
-    File nounlist;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class WindwardHacker implements PasswordGuesser{
+    private Scanner nounlist;
+    private char[] specialCharacters;
+    private int index;
+    private String currentWord;
+    private int yearModifier;
+
     public WindwardHacker(){
+        specialCharacters = new char[]{'!', '@', '#', '$', '%', '^', '&', '*',};
+        index = 0;
+        yearModifier = 0;
+
+        try{
+            nounlist = new Scanner(getClass().getResourceAsStream("nounlist"));
+        }catch(Exception e){}
+
+        if(nounlist.hasNext()) {
+            currentWord = nounlist.next();
+        }
+    }
+
+    public String getNext(){
+        String password = currentWord + specialCharacters[index];
+
+        //gets year, then adds 1 to it if it is July or later, then adds the current year we are testing
+        password += Calendar.getInstance().get(Calendar.YEAR) +  (Calendar.getInstance().get(Calendar.MONTH)+5)%11 + yearModifier;
+
+        if (++yearModifier == 7){
+            yearModifier = 0;
+            if(++index == specialCharacters.length){
+                index = 0;
+                if(nounlist.hasNext()){
+                    currentWord = nounlist.next();
+                }else{
+                    return null;
+                }
+            }
+        }
+        return password;
     }
 }
