9a3b8e50946c876c109baa0ee323b564cbf934ac
bbishop19 <bbishop19@windwardschool.org>
2018-10-26 00:04:53 -0700

    create info screen

--- src/view/View.java
+++ src/view/View.java
@@ -2,15 +2,14 @@ package view;
 
 import java.util.Arrays;
 
-import controller.Controller;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class View extends Application{	
@@ -19,16 +18,16 @@ public class View extends Application{
 	}
 	
 	public void start(Stage stage) throws Exception {
-		Group root = new Group();
+		TilePane mainMenuRoot = new TilePane();
 
 		PasswordField password = new PasswordField();
 		password.setPromptText("Enter password here");
-		password.setPrefSize(200, 30);
+		password.setPrefSize(300, 30);
 		password.setAlignment(Pos.CENTER);
 
+
 		Button wwh = new Button("Windward Hack");
-		wwh.setPrefSize(200, 30);
-		wwh.relocate(0, 30);
+		wwh.setPrefSize(300, 30);
 		wwh.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
                 //todo
@@ -36,8 +35,7 @@ public class View extends Application{
         });
 
 		Button ch = new Button("Common Passwords");
-        ch.setPrefSize(200, 30);
-		ch.relocate(0, 60);
+        ch.setPrefSize(300, 30);
         ch.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
                 //todo
@@ -45,17 +43,46 @@ public class View extends Application{
         });
 
 		Button bf = new Button("Brute Force");
-        bf.setPrefSize(200, 30);
-		bf.relocate(0,  90);
+        bf.setPrefSize(300, 30);
         bf.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
                 //todo
             }
         });
 
-		root.getChildren().addAll(Arrays.asList(password, wwh, ch, bf));
-		
-		stage.setScene(new Scene(root, 200, 120));
+		mainMenuRoot.getChildren().addAll(password, wwh, ch, bf);
+		Scene mainMenu = new Scene(mainMenuRoot, 300, 120);
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+		TilePane infoRoot = new TilePane();
+		infoRoot.setPrefSize(300, 120);
+		infoRoot.setAlignment(Pos.CENTER);
+
+
+		Text crackedMessage = new Text("Your password was cracked in");
+
+		Text timeMessage = new Text("1234 ms and");
+
+		Text attemptsMessage = new Text("5678 attempts");
+
+		Button backButton = new Button("Test again");
+		backButton.setPrefSize(300, 30);
+		backButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				stage.setScene(mainMenu);
+			}
+		});
+
+
+		infoRoot.getChildren().addAll(crackedMessage, timeMessage, attemptsMessage, backButton);
+
+		Scene infoScreen = new Scene(infoRoot, 300, 120);
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+		stage.setScene(infoScreen);
+		stage.setResizable(false);
 		stage.show();
 	}
 }
